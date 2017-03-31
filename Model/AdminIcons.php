7d7c6c52fe76a1b4abe8d92bceb0e070ffda4e04
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-03-31 10:40:15 +0200

    Update icons

--- Model/AdminIcons.php
+++ Model/AdminIcons.php
@@ -11,16 +11,17 @@ namespace Lch\ComponentsBundle\Model;
 
 class AdminIcons
 {
-    const DASHBOARD = "glyphicon glyphicon-dashboard";
-    const DOWNLOAD_ALT = "glyphicon glyphicon-download-alt";
-    const DUPLICATE = "glyphicon glyphicon-duplicate";
-    const EDIT = "glyphicon glyphicon-edit";
-    const EQUALIZER = "glyphicon glyphicon-equalizer";
-    const EYE_OPEN = "glyphicon glyphicon-eye-open";
-    const EYE_CLOSED = "glyphicon glyphicon-eye-close";
-    const PENCIL = "glyphicon glyphicon-pencil";
-    const REPEAT = "glyphicon glyphicon-repeat";
-    const SCREENSHOT = "glyphicon glyphicon-screenshot";
-    const SIGNAL = "glyphicon glyphicon-signal";
-    const TASKS = "glyphicon glyphicon-tasks";
+    const DASHBOARD         = "glyphicon glyphicon-dashboard";
+    const DOWNLOAD_ALT      = "glyphicon glyphicon-download-alt";
+    const DUPLICATE         = "glyphicon glyphicon-duplicate";
+    const EDIT              = "glyphicon glyphicon-edit";
+    const EQUALIZER         = "glyphicon glyphicon-equalizer";
+    const EYE_OPEN          = "glyphicon glyphicon-eye-open";
+    const EYE_CLOSED        = "glyphicon glyphicon-eye-close";
+    const HAMBURGER         = "glyphicon glyphicon-menu-hamburger";
+    const PENCIL            = "glyphicon glyphicon-pencil";
+    const REPEAT            = "glyphicon glyphicon-repeat";
+    const SCREENSHOT        = "glyphicon glyphicon-screenshot";
+    const SIGNAL            = "glyphicon glyphicon-signal";
+    const TASKS             = "glyphicon glyphicon-tasks";
 }
